90310cadd9f2187e103419e2afd938ed6810d0d2
Alexander Feodorov <vester@unixcomp.org>
2023-01-28 11:43:34 +0400

    Fix

--- test/lab1.f95
+++ test/lab1.f95
@@ -2,8 +2,14 @@ program test_lab1
   use recursion
   use cycle
 
-  ix1 = 2
-  ix2 = 3
+  ix1 = 0
+  ix2 = 0
+
+  print *, "Input X1 value: "
+  read '(I4)', ix1
+
+  print *, "Input X2 value: "
+  read '(I4)', ix2
 
   icyc = cyc(ix1, ix2)
   irec = rec(ix1, ix2)
